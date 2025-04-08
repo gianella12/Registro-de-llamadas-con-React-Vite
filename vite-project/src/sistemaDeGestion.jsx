b3b6e4dae1e75e4ce7b8a99ae8a8fe6a9adad6b4
gianella lastra <gianellalastra4@gmail.com>
2025-04-08 16:50:53 -0300

    Modifico una validacion del componente

--- vite-project/src/sistemaDeGestion.jsx
+++ vite-project/src/sistemaDeGestion.jsx
@@ -20,11 +20,10 @@ export function ManejoDeLlamadas({ llamadas }) {
 
   function manejarCambio(event, campo){
     const nuevoValor = event.target.value;
-    if(nuevoValor = 10){
     setValoresEditados({
       ...valoresEditados,
       [campo]: nuevoValor
-    });}
+    });
   }
   
   function guardarCambio(index, campo) {
